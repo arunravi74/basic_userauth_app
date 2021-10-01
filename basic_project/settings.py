abcabc194394dd0fe14bea34adc5c8b9a5ce5855
Arun kumar <arunravi.74@gmail.com>
2021-10-01 22:35:20 +0530

    Updated_Seeting

--- basic_project/settings.py
+++ basic_project/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wgi%jf_4b2&52&jurx0lt)7$bgq8soy5$1wfg^01p7c&^zuvni'
+#SECRET_KEY = 'django-insecure-wgi%jf_4b2&52&jurx0lt)7$bgq8soy5$1wfg^01p7c&^zuvni'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['userauthdjangoapp.herokuapp.com']
 
 
 # Application definition
